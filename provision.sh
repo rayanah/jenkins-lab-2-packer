21a2f44e95391b8705fa10935f273c94a78f97b0
rayanah <ror.s.m2010@hotmail.com>
2020-08-13 10:10:21 +0300

    nginx try 5

--- provision.sh
+++ provision.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 sudo apt-get update -y -q
-sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
+sudo apt update -y && sudo apt install -y httpd curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
-            sudo apt install -y httpd
 	    sleep 10
 	    cd /var/jenkins_home/workspace/rayanah-tr-pipeline
 	    mv ip_address.txt index.html
 	    cat index.html > /var/www/html/index.html
-            systemd start httpd
-            systemd enable httpd
+            systemctl start httpd
+            systemctl enable httpd
+
+
