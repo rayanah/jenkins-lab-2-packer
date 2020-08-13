a2d4079ad0eda8fd8a30ecd6fa16850bdd9cafa0
rayanah <ror.s.m2010@hotmail.com>
2020-08-13 11:54:45 +0300

    nginx try 7

--- Makefile
+++ Makefile
@@ -14,15 +14,13 @@ start:
 		--env AWS_ACCESS_KEY_ID="$$(sed -n 2p creds/credentials | sed 's/.*=//')" \
 		--env AWS_SECRET_ACCESS_KEY="$$(sed -n 3p creds/credentials | sed 's/.*=//')" \
 		--env OWNER=$$OWNER \
+		--env PROJECT_NAME=$$PROJECT_NAME \
 		-v /var/run/docker.sock:/var/run/docker.sock \
 		-v $$PWD:/$$(basename $$PWD) \
-		-v k3s_packer:/token \
-		-v /some/content:/usr/share/nginx/html:ro \
 		-w /$$(basename $$PWD) \
 		--name $$(basename $$PWD) \
 		--hostname $$(basename $$PWD) \
-		-p 8080:80 \
-		nginx
+		bryandollery/terraform-packer-aws-alpine bash
 
 stop:
 	docker rm -f $$(basename $$PWD) 2> /dev/null || true
--- provision.sh
+++ provision.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 sudo apt-get update -y -q
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
-sudo usermod -aG docker ubuntu
-	    sleep 10
-	    cd /var/jenkins_home/workspace/rayanah-tr-pipeline
-	    mv ip_address.txt index.html
-	    cat index.html > /var/www/html/index.html
-            systemctl start httpd
-            systemctl enable httpd
+sudo usermod -aG docker ubuntusudo mkdir -p /home/ubuntu/api
+
+sudo chmod a+rwx /home/ubuntu/api
+sudo chown ubuntu:ubuntu /home/ubuntu/api
+
+
+sudo touch /etc/systemd/system/webserver.service
+sudo chmod a+rw /etc/systemd/system/webserver.service
+cat <<EOF > /etc/systemd/system/webserver.service
+[Unit]
+Description=Nginx API address server
+After=docker.service
+[Service]
+Type=simple
+ExecStart=sudo docker container run -d --name nginx -p 80:80 --restart=always -v /home/ubuntu/api/:/usr/share/nginx/html:ro nginx
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo chmod a+r /etc/systemd/system/webserver.service
+sudo systemctl daemon-reload
+sudo systemctl enable webserver
+sudo systemctl start webserver
 
 
